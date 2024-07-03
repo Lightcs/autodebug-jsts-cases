c54e55f48174a3ea3c81e092399f92692648ad9f
caoyunxiang <caoyunxiang@bytedance.com>
2024-07-03 13:57:29 +0000

    js: base_002_export_error

---
+++ main.js
@@ -0,0 +1,9 @@
+const lib = require('./lib');
+const arr = [5, 3, 8, 4, 2, 1, 9, 7, 6];
+const sortedArr = lib.quickSort(arr);
+
+console.log(sortedArr);
+
+if (JSON.stringify(sortedArr) !== JSON.stringify([1, 2, 3, 4, 5, 6, 7, 8, 9])) {
+    throw new Error('Unsorted array');
+}
